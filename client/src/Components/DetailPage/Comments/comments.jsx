6fe95f9684182054cc9f33b63602cce56ebdb49a
Yifan Cao <amadeuscao.cs@gmail.com>
2019-05-02 12:30:24 -0500

    add files

---
+++ client/src/Components/DetailPage/Comments/comments.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Card, Button, Label} from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {commentsCss, commentClickCss, commentTitle} from './comments.module.scss'
+
+class Comments extends Component {
+    constructor() {
+        super();
+        this.state = {            
+            comments: [],                                       //all comments and corresponding users of the game
+            addComment: '',                                     //the comment added by user
+            commentClickDisplay: 'block',                       //add comment link, not visible when a comment box appears
+            commentBoxDisplay: 'none',                          //comment box, visible when add comment link is clicked
+            buttonDisplay: 'none',                              //submit and cancel button, visible when add comment link is clicked
+        }
+        this.addComment = this.addComment.bind(this);           //click to add a comment box
+        this.writeComment = this.writeComment.bind(this);       //input change handler of the comment box
+        this.submit = this.submit.bind(this);                   //submit comment handler
+        this.cancel = this.cancel.bind(this);                   //cancel commenting handler
+    }
+
+    componentDidMount() {
+        axios.get('api/game/' + this.props.appid)
+        .then(response => {
+            this.setState({          
+                comments: response.data.data.comment
+            })
+        }).catch(err => {
+            alert(err)
+        })
+    }
+
+    addComment() {
+        this.setState({
+             commentBoxDisplay: 'block',
+             buttonDisplay: 'inline',
+             commentClickDisplay: 'none',
+        })
+     }
+
+     //write the comment into the text box
+     writeComment(event) {
+         if (event.target.value.length >= 1000) {
+             alert('Reach maximum character limits');
+             return;
+         }
+         this.setState({
+             addComment: event.target.value,
+         })
+     }
+
+    //submit the comment, axios post should be added here 
+    submit() {
+        let commentList = this.state.comments;
+        commentList.push(this.state.addComment);
+        if (this.state.addComment === '') {
+            alert('Please add comments');
+            return;
+        }
+        this.setState ({
+             commentBoxDisplay: 'none',
+             buttonDisplay: 'none',
+             addComment: '',
+             comments: commentList,
+             commentClickDisplay: 'block',
+         })
+        axios.put('api/game/comment/' + this.props.appid, {
+            comment: this.state.addComment,
+            user_name: "test_name",
+            user_id: "test_id",
+        })
+     }
+
+     //cancel the comment
+     cancel() {
+         this.setState({
+             commentBoxDisplay: 'none',
+             buttonDisplay: 'none',
+             addComment: '',
+             commentClickDisplay: 'block',
+         })
+     }
+
+     render() {
+        //Add a comment is invisible when a comment box is visible
+        let commentClickStyle = {
+            display: this.state.commentClickDisplay
+        }
+         //comment box is visible when add a comment is clicked
+        let commentBoxStyle = {
+            display: this.state.commentBoxDisplay
+        }
+        //button is visible when add a comment is clicked
+        let buttonDisplayStyle = {
+            display: this.state.buttonDisplay
+        }
+        let commentLists = this.state.comments.map(currComment => {
+            return (
+                <Label>{currComment.user_name}:{currComment.comment}</Label>
+            );
+        })
+
+        return(
+            <Card>
+                <span className = {commentTitle}>Comments</span>
+                <Card.Content className = {commentsCss}>
+                    <Card.Header className = {commentClickCss} onClick = {this.addComment} style = {commentClickStyle}>
+                        <FontAwesomeIcon icon="comment-alt"/> Add a comment
+                    </Card.Header>
+                    {commentLists}
+                    <textarea name="commentBox" maxlength = '1000' onChange = {this.writeComment} 
+                    value = {this.state.addComment} style = {commentBoxStyle}></textarea>
+                    <Button onClick = {this.submit} style = {buttonDisplayStyle}>Submit</Button>  
+                    <Button onClick = {this.cancel} style = {buttonDisplayStyle}>Cancel</Button>
+                </Card.Content>
+            </Card>
+        )
+     }
+}
+
+export default Comments;
---
+++ client/src/Components/DetailPage/Introduction/intro.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import ReactHtmlParser from 'react-html-parser';
+import {Card} from 'semantic-ui-react';
+import HorizontalBar from './bar.jsx';
+import {imageSection, text, score, image, intro, 
+    introTitle, introContent, introClick} from './intro.module.scss';
+
+
+class Introduction extends Component {
+    constructor() {
+        super();
+        this.state = {
+            game: {},
+            detailIntro: '',                                   //detail intro of a game
+            briefIntro: '',                                     //brief intro of a game
+            displayIntro: '',                                   //display detail or bried intro, depending on show more or less link, by default is brief intro of a game
+            clickLink: 'show more',                             //whether to show detail description or brief description
+        }
+        this.showContent = this.showContent.bind(this);         //click handler of show more/less link
+    }
+
+    componentDidMount(){
+        let htmlContent = this.props.game[this.props.appid].data.detailed_description;
+        let content = ReactHtmlParser(htmlContent);
+        let shortContent = this.props.game[this.props.appid].data.short_description;
+        this.setState({
+            game: this.props.game,
+            detailIntro: content,
+            briefIntro: shortContent, 
+            displayIntro: shortContent,
+        })
+    }
+
+     //click show more or show less to show detail or brief content
+     showContent() {
+        if (this.state.clickLink === 'show more') {
+            let content = this.state.detailIntro;
+            this.setState({
+                displayIntro: content,
+                clickLink: 'show less',
+            })
+        } else {
+            let content = this.state.briefIntro;
+            this.setState({
+                displayIntro: content,
+                clickLink: 'show more',
+            })
+        }
+    }
+
+    render() {
+        let imageUrl = this.props.game[this.props.appid].data.header_image;
+        let publishers = this.props.game[this.props.appid].data.publishers;
+        let isReleased = this.props.game[this.props.appid].data.release_date.coming_soon;
+        let categories = this.props.game[this.props.appid].data.categories;
+        let genres = this.props.game[this.props.appid].data.genres;
+        if (categories.length > 3) {                
+            categories = categories.slice(0, 3);
+        }
+        categories = categories.map(category => {
+            return <li>{category.description}</li>
+        })
+        if (genres.length > 3) {
+            genres = genres.slice(0, 3);
+        }
+        genres = genres.map(genre => {
+            return <li>{genre.description}</li>
+        })
+        var releaseDate;
+        if (isReleased) {
+            releaseDate = 'Coming Soon';
+        } else {
+            releaseDate = this.props.game[this.props.appid].data.release_date.date;
+        }
+        let linkStyle = {
+            display: 'block'
+        }
+        if (this.state.detailIntro === this.state.briefIntro) {
+            linkStyle = {
+                display: 'none'
+            }
+        }
+
+        return(
+            <div>
+                <div className = {imageSection}>
+                    <img className = {image} src = {imageUrl} alt = 'game scene'/>
+                    <div className = {text}>
+                        Publishers: {publishers} <br/>
+                        Release Date: {releaseDate}<br/>
+                        Categories: {categories}
+                        Genres: {genres}
+                    </div>
+                    <div className = {score}>
+                        Average Score: 8.8
+                        <HorizontalBar/>
+                    </div>
+                </div>
+                <Card>
+                    <Card.Content className = {intro}>
+                        <Card.Header className = {introTitle}>
+                            Introduction
+                        </Card.Header>
+                        <div className = {introContent}>
+                            {this.state.displayIntro}
+                        </div>
+                        <span className = {introClick} onClick = {this.showContent} 
+                        style = {linkStyle}>{this.state.clickLink}</span>
+                    </Card.Content>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default Introduction;
